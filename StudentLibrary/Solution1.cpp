a6a76f1c6b943226c062396aeaecc59e3c9bbc27
unknown <mbahri@gamingcampus.local>
2024-01-19 13:32:59 +0100

    Les calculs sont bons mais il fait pas la bonne approximation donc ca passe pas les tests jcrois

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,4 +1,5 @@
 #include "Solution1.h"
+#include <iostream>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -7,22 +8,22 @@
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
 	if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::FAHRENHEIT) {
-		return 9 / 5 * _value + 32;
+		return 9.f / 5.f * _value + 32.f;
 	}
 	else if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::FAHRENHEIT) {
-		return 9 / 5 * (_value - 273) + 32;
+		return 9.f / 5.f * (_value - 273.f) + 32.f;
 	}
 	else if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::CELSIUS) {
-		return 5 / 9 * (_value - 32);
+		return 5.f / 9.f * (_value - 32.f);
 	}
 	else if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::KELVIN) {
-		return _value + 273;
+		return _value + 273.f;
 	}
 	else if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::CELSIUS) {
-		return _value - 273;
+		return _value - 273.f;
 	}
 	else if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::KELVIN) {
-		return 5 / 9 * (_value - 32) + 273;
+		return 5.f / 9.f * (_value - 32.f) + 273.f;
 	}
 
 	return -1.0f;
