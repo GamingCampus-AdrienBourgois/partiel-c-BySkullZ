1dba9c6ffad73a880e8e17d6f85ee9cb8da84b31
unknown <mbahri@gamingcampus.local>
2024-01-19 13:42:55 +0100

    Exo 2 peut etre

--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -1,5 +1,8 @@
 #include "Solution2.h"
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <ostream>
 
 // Don't forget to enable the exercise in the SudentConfiguration.h file !
@@ -8,6 +11,37 @@
 
 float Solution2::GetBalance(const std::string& accountName)
 {
+	std::ifstream account(accountName);
+
+    float balance = 0.f;
+
+    if (account.is_open()) {
+        std::string ligne;
+        while (std::getline(account, ligne)) {
+            std::istringstream ss(ligne);
+            std::string operation;
+            float amount;
+
+            ss >> operation >> amount;
+
+            if (operation == "DEPOSIT") {
+                balance += amount;
+            }
+            else if (operation == "WITHDRAW") {
+                balance -= amount;
+            }
+            else {
+                std::cerr << "Opération non reconnue : " << operation << std::endl;
+            }
+        }
+
+        account.close();
+
+        return balance;
+    }
+    
+    throw std::exception("Compte non valide.");
+
 	return -1.0f;
 }
 
